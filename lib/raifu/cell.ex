4cc8a4eaa78a103997e75ae6b74545918a93d123
Samuel Monroe <spat.monroe@gmail.com>
2022-06-11 09:52:48 +0200

    Public API and test alive message

--- lib/raifu/cell.ex
+++ lib/raifu/cell.ex
@@ -1,4 +1,32 @@
 defmodule Raifu.Cell do
+  @behaviour GenServer
+  # Public API
+  def start_link({position, _width, _length} = opts) do
+    GenServer.start_link(__MODULE__, opts, name: cell_name(position))
+  end
+
+  def alive?(name) do
+    GenServer.call(name, :alive)
+  end
+
+  def cell_name(position) do
+    pos = position |> Tuple.to_list() |> Enum.join()
+    "cell#{pos}" |> String.to_atom()
+  end
+
+  # GenServer Callbacks
+  @impl true
+  def init({position, width, length}) do
+    neighbors = compute_neighborhood(position, width, length)
+    alive = Enum.random([1,2]) == 2
+    {:ok, {alive, neighbors}}
+  end
+
+  @impl true
+  def handle_call(:alive, _from, {alive, neighbors}) do
+    {:reply, alive, {alive, neighbors}}
+  end
+
   def compute_neighborhood({x,y}, width, length) do
     for m <- x-1..x+1, n <- y-1..y+1 do
       {m,n}
--- test/raifu/cell_test.exs
+++ test/raifu/cell_test.exs
@@ -2,6 +2,16 @@ defmodule RaifuTest.Cell do
   use ExUnit.Case
   alias Raifu.Cell
 
+  describe "alive/1" do
+    test "returns the livingness status of the cell" do
+      position = {1,1}
+      name = Cell.cell_name(position)
+      start_supervised!(%{id: Cell, start: {Cell, :start_link, [{position, 2, 2}]}})
+
+      assert Cell.alive?(name) in [true, false]
+    end
+  end
+
   describe "compute neighborhood/3" do
     test "determine the proper set of neighbors for corner cells" do
       assert lists_are_equals?(Cell.compute_neighborhood({0,0}, 2, 2), [{0,1},{1,0}, {1,1}])
