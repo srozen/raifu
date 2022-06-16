344c1f52f879530eaffd481d42d9a2bee201d0a3
Samuel Monroe <spat.monroe@gmail.com>
2022-06-16 07:34:19 +0200

    Board is a simple GenServer

--- lib/raifu/board.ex
+++ lib/raifu/board.ex
@@ -1,14 +1,14 @@
 defmodule Raifu.Board do
-  use DynamicSupervisor
+  use GenServer
 
   ## Public API
   def start_link(opts) do
-    DynamicSupervisor.start_link(__MODULE__, opts, name: __MODULE__)
+    GenServer.start_link(__MODULE__, opts, name: __MODULE__)
   end
 
   ## Implementation
   @impl true
-  def init(_opts) do
-    DynamicSupervisor.init(strategy: :all_for_one)
+  def init(opts) do
+    {:ok, opts}
   end
 end
